54223b1fe2e33288feae03604d7d20fc517fb657
unknown <jdanielrojas18@gmail.com>
2024-07-18 13:17:56 -0400

    L

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,6 @@
 import AboutUs from '@/pages/home/AboutUs'
 import Client from '@/pages/home/Client'
-import { Hero } from '@/pages/home/Hero'
+import  Hero  from '@/pages/home/Hero'
 import Image from '@/pages/home/Image'
 import Services from '@/pages/home/Services'
 import WhyUs from '@/pages/home/WhyUs'
